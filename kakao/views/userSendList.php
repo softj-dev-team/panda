68248f2717034641241dac9c8d2c8f87b07adfad
devuser <dev@softj.net>
2024-08-28 02:21:41 +0900

    send list 30

--- kakao/views/userSendList.php
+++ kakao/views/userSendList.php
@@ -29,8 +29,8 @@
                 <colgroup>
 
                     <col style="width:20%;">
-                    <col style="width:20%">
-                    <col style="width:40%">
+                    <col style="width:25%">
+                    <col style="width:30%">
                     <col style="width:20%">
 
                 </colgroup>
