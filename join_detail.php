0d8a4bc36e17ae2589f212f47916e17d39c68a90
devuser <dev@softj.net>
2024-08-28 20:26:52 +0900

    휴대폰인증 1

--- join_detail.php
+++ join_detail.php
@@ -348,7 +348,7 @@
 			}
 
 			if (res_cd != "0000") {
-				alert("인증에 실패 하였습니다.");console.log(frm.elements)
+				alert("인증에 실패 하였습니다."+frm.elements);console.log(frm.elements)
 				return;
 			} else {
 				alert("인증되었습니다.");
