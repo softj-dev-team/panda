d651b3799fb5d2aa0a0ff8a1ac0cbdcc0bcf730d
devuser <dev@softj.net>
2024-10-02 20:26:03 +0900

    db 연결 수정.

--- pro_inc/db_conn.php
+++ pro_inc/db_conn.php
@@ -1,6 +1,6 @@
 <?php
 //	$gconnet = mysqli_connect("192.168.219.110","asssahcom9","soulvocal7!!","asssahcom9","13306");
-	$gconnet = mysqli_connect("localhost","asssahcom9","soulvocal7!!","asssahcom9","3306");
+	$gconnet = mysqli_connect("139.150.69.187","asssahcom9","soulvocal7!!","asssahcom9","3306");
 	mysqli_query($gconnet,"set names UTF8");
 	
 ?>
