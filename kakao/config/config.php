826eaee53c21f5dd2d22f3683e144c8ec5e0d62b
devuser <dev@softj.net>
2024-10-02 20:27:08 +0900

    db 연결 수정.

--- kakao/config/config.php
+++ kakao/config/config.php
@@ -5,7 +5,7 @@
 //define('DB_NAME', 'pandakko');
 //define('DB_USER', 'pandakko');
 //define('DB_PASS', '!1qazsoftj');
-define('DB_HOST', '139.150.69.187');
+define('DB_HOST', 'localhost');
 define('DB_NAME', 'asssahcom9');
 define('DB_USER', 'asssahcom9');
 define('DB_PASS', 'soulvocal7!!');
